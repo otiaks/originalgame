30208aff8cadbbb635f17b8f8d61bc9d95b80085
unknown <>
2016-06-25 04:12:44 +0900

    α版

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	
+public GameObject explosion;
+	public AudioSource Bomb;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "bullet") {
+			Destroy (this.gameObject);
+			Instantiate (explosion, transform.position, Quaternion.identity);
+			Bomb.Play ();
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Shot : MonoBehaviour {
+	public GameObject bullet; 
+	public Transform spawn;   
+	public float speed ;
+	public AudioSource gunSound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (Input.GetMouseButtonDown (0)) {
+			shot (); 
+			gunSound.Play ();
+		}
+		*/
+	}
+
+	public void StartButton(){
+		shot(); 
+		gunSound.Play ();
+	}
+		
+	void shot(){
+		GameObject obj = GameObject.Instantiate(bullet);
+		obj.transform.position = spawn.position;
+		obj.GetComponent<Rigidbody>().velocity = new Vector3 (1, 0, 0)*speed;
+	}
+
+}
+
+
